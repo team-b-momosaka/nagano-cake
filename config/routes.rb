a5802ee4eb2c03579c4fa2a195d0a7f1e648cf62
SHUNPEI3 <119581004+SHUNPEI3@users.noreply.github.com>
2023-01-17 17:49:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-
+<<<<<<< feature-Add_Controller
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+=======
+  namespace :public do
+    get 'addresses　index/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+>>>>>>> develop
   end
 # 顧客用
 # URL /customers/sign_in ...
