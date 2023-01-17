1bc19914c7d208cdb62df76cef067440b4b31a00
kamishubaizumo <84670496+kamishubaizumo@users.noreply.github.com>
2023-01-17 08:19:10 +0000

    add controller-action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
