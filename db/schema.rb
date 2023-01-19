32b2649a184ea380453e9e67d320e07516ad118d
EC2 Default User <ec2-user@ip-172-31-22-208.ap-northeast-1.compute.internal>
2023-01-19 07:56:28 +0000

    postal_codeをfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_023832) do
+ActiveRecord::Schema.define(version: 2023_01_19_074851) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2023_01_17_023832) do
     t.string "first_name_kana", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "pastal_code", null: false
+    t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
     t.boolean "is_deleted", null: false
