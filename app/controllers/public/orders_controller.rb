12c4bc04883114a7f474ca693b3db176844d3c34
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-01-22 07:32:57 +0000

    [Update]顧客側注文情報入力画面ページのレイアウト作成(２回目)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,12 +2,22 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
-  
+
+  def comfirm
+    # binding.pry
+    @order = Order.new(order_params)
+  end
 
   def index
   end
 
   def show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, )
+  end
+
 end
