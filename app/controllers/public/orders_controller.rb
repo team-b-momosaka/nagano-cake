f2e96e935103db91eebaf0f37fb8c5325586cf9e
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-01-22 10:37:42 +0000

    [Update]注文情報確認画面ページのレイアウト作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,9 +4,33 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
-    # binding.pry
     @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.shipping_postcode = current_customer.postal_code
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.shipping_postcode = @address.postcode
+      @order.shipping_address = @address.addresses
+      @order.shipping_name = @address.name
+
+    elsif params[:order][:select_address] == "2"
+      @order.shipping_postcode = params[:order][:shipping_postcode]
+      @order.shipping_address = params[:order][:shipping_address]
+      @order.shipping_name = params[:order][:shipping_name]
+    end
+    #binding.pry ←デバック用
   end
+
+  def create
+    redirect_to orders_complete_path
+  end
+
+  def complete
+  end
+
 
   def index
   end
@@ -17,7 +41,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, )
+    params.require(:order).permit(:payment_method, :shipping_postcode, :shipping_address, :shipping_name)
   end
 
 end
