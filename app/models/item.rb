6ac07675859933f791e7f2e4eae4809bd23525c0
kamishubaizumo <kamishubaizumo2060@gmail.com>
2023-01-23 02:58:33 +0000

    view item index show

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
   has_one_attached :image
+
+
+  #税込み価格表示
+  def add_tax_sales_price
+    (self.no_tax_price * 1.10).round
+  end
+
+
+
 end
