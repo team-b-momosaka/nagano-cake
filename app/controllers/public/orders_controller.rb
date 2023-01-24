b48c602365f97452d9f967aa61886cfe289f9333
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2023-01-24 06:19:48 +0000

    [Finish]顧客側注文確定機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -36,7 +36,7 @@ class Public::OrdersController < ApplicationController
         order_detail = OrderDetail.new
         order_detail.order_id = @order.id
         order_detail.item_id = cart_item.item_id
-        order_detail.purchase_price = (cart_item.item.no_tax_price * 1.1).floor
+        order_detail.purchase_price = cart_item.item.add_tax_sales_price
         order_detail.quantity = cart_item.merchandise_quantity
         order_detail.save
       end
