f89217fa1619cd746c7b37d6d1964e67e7f43782
EC2 Default User <ec2-user@ip-172-31-1-134.ap-northeast-1.compute.internal>
2023-01-25 07:04:27 +0000

    validation

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,6 +9,8 @@ class Admin::GenresController < ApplicationController
     if genre.save
       redirect_to admin_genres_path
     else
+      @genres = Genre.all
+      @genre = Genre.new
       render "index"
     end
   end
@@ -22,6 +24,7 @@ class Admin::GenresController < ApplicationController
     if genre.update(genre_params)
       redirect_to admin_genres_path
     else
+      @genre = Genre.find(params[:id])
       render "edit"
     end
   end
