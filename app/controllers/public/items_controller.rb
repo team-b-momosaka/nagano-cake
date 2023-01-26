8ed48815a16049418fbb6adb1d2550452e4231fc
kamishubaizumo <kamishubaizumo2060@gmail.com>
2023-01-26 05:35:41 +0000

    ジャンル絞り

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,26 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
-    #ページネーション用に書き換えた。8個まで表示する。
-    @item = Item.all
 
-    @genres = Genre.all
+    #ページネーション用に書き換えた。8個まで表示する。
+    if params[:genre_id].blank? # genre_idが空かどうか blank?でチェック。
+
+      @items = Item.page(params[:page]).per(8)
+      @items_all = Item.all.count #countで、余分なデータを限定
+      @genres = Genre.all
+      @index = "商品"
+
+    else
+       # ジャンルを押したら
+        #そのジャンルのアイテムが表示されるようにする
+
+        @genre = Genre.find(params[:genre_id]) #viewのlink_toで、送った(params[:genre_id])を使い、genreを一件持ってくる
+        @items = @genre.items.page(params[:page]).per(8) #ジャンルを一件持ってきた後、そのIDををもってるアイテムを呼ぶ
+        @items_all = @genre.items.count #カウント用にジャンルの全件データ取得
+        @genres = Genre.all   #ジャンル一覧
+        @index = @genre.genre #@genreでジャンルの名前を入れる
+      end
+
+
 
   end
 
@@ -14,8 +30,6 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
   end
 
-  def search
-  end
 
 
 end
